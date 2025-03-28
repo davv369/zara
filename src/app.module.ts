8a3763db4cf943d79a7dc249237e067eb5da9112
GitLab <dawidpiasta@crustlab.com>
2025-03-28 20:55:20 +0100

    force redeploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,45 +18,13 @@ import { ScraperModule } from './scraper/scraper.module';
       entities: [ProductEntity],
       synchronize: true,
       extra: {
-        connectionTimeout: 10000, // 10 sekund timeout
-      }
-    }),
+        ssl: {
+          rejectUnauthorized: false
+        },
+      }}),
     TypeOrmModule.forFeature([ProductEntity]),
     ScraperModule,
   ],
   providers: [ScraperService, ProductConnector, CategoryConnector, DetailsConnector],
 })
 export class AppModule {}
-
-
-
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ScraperService } from './scraper/scraper.service';
-// import { ProductConnector, CategoryConnector, DetailsConnector } from './connectors/connectos';
-// import { ProductEntity } from './models/product.entity';
-// import { ScraperModule } from './scraper/scraper.module';
-// import {ConfigModule} from "@nestjs/config";
-//
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({
-//       isGlobal: true,
-//     }),
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       host: porcess.env.,
-//       port: 5432,
-//       username: 'new_postgres',
-//       password: 'newPassword123',
-//       database: 'zara_db',
-//       entities: [ProductEntity],
-//       synchronize: true, // Ensure this is set to true
-//
-//     }),
-//     TypeOrmModule.forFeature([ProductEntity]),
-//     ScraperModule,
-//   ],
-//   providers: [ScraperService, ProductConnector, CategoryConnector, DetailsConnector],
-// })
-// export class AppModule {}
