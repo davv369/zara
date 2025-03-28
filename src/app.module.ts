e10fa0c399fcff78cb6bab0d4d9a1cf3c3db18b2
GitLab <dawidpiasta@crustlab.com>
2025-03-28 20:32:19 +0100

    ssl require true

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,12 +13,15 @@ import { ScraperModule } from './scraper/scraper.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL, // Używamy pełnego URL z Railway
-      ssl: { rejectUnauthorized: false }, // Wymagane dla Railway
+      url: process.env.DATABASE_URL,
+      ssl: {
+        require: true, // <-- NOWA KONFIGURACJA
+        rejectUnauthorized: false
+      },
       entities: [ProductEntity],
       synchronize: true,
       extra: {
-        connectionTimeout: 10000, // 10 sekund timeout
+        connectionTimeout: 10000, /
       }
     }),
     TypeOrmModule.forFeature([ProductEntity]),
@@ -27,3 +30,36 @@ import { ScraperModule } from './scraper/scraper.module';
   providers: [ScraperService, ProductConnector, CategoryConnector, DetailsConnector],
 })
 export class AppModule {}
+
+
+
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ScraperService } from './scraper/scraper.service';
+// import { ProductConnector, CategoryConnector, DetailsConnector } from './connectors/connectos';
+// import { ProductEntity } from './models/product.entity';
+// import { ScraperModule } from './scraper/scraper.module';
+// import {ConfigModule} from "@nestjs/config";
+//
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//     }),
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: porcess.env.,
+//       port: 5432,
+//       username: 'new_postgres',
+//       password: 'newPassword123',
+//       database: 'zara_db',
+//       entities: [ProductEntity],
+//       synchronize: true, // Ensure this is set to true
+//
+//     }),
+//     TypeOrmModule.forFeature([ProductEntity]),
+//     ScraperModule,
+//   ],
+//   providers: [ScraperService, ProductConnector, CategoryConnector, DetailsConnector],
+// })
+// export class AppModule {}
